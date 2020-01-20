58cb50f7415a45ae1cc202ab122cade0daa76104
damithc <damith@gmail.com>
2020-01-20 20:26:23 +0800

    Extract a LoanManager class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,19 @@
 public class Main {
-    static Loan[] loans = new Loan[100];
-    static int loansCount = 0;
-
-    public static void addLoan(String borrower, int amount, double interestRate){
-        loans[loansCount] = new Loan(borrower, amount, interestRate);
-        loansCount++;
-    }
-
-    public static void setAsPaid(String borrower){
-        for (int i=0; i< loansCount; i++){
-            if (loans[i].getBorrower().equals(borrower)){
-                loans[i].setAsPaid();
-            }
-        }
-    }
-
-    public static void listLoans(){
-        for (int i=0; i< loansCount; i++){
-            if (!loans[i].isPaid()){
-                System.out.println(loans[i].getDisplayString());
-            }
-        }
-    }
-
-    public static double calculateProfitEarned(){
-        double profit = 0;
-        for (int i=0; i< loansCount; i++){
-            // if the loan was repaid, add the interest to profit
-            if (loans[i].isPaid()){
-                profit = profit + loans[i].getProfit();
-            }
-        }
-        return profit;
-    }
 
     public static void main(String[] args) {
-        addLoan("Alice Betsy", 10, 0.1);
-        addLoan("Ben Chee", 20, 0.2);
-        addLoan("Chris Davis", 30, 0.3);
-        setAsPaid("Ben Chee");
-        listLoans();
-        System.out.println("profit: " + calculateProfitEarned());
+        LoanManager m1 = new LoanManager();
+        m1.addLoan("Alice Betsy", 10, 0.1);
+        m1.addLoan("Ben Chee", 20, 0.2);
+        m1.addLoan("Chris Davis", 30, 0.3);
+        m1.setAsPaid("Ben Chee");
+        m1.listLoans();
+        System.out.println("profit from m1: " + m1.calculateProfitEarned());
+        
+        LoanManager m2 = new LoanManager();
+        m2.addLoan("Alice Betsy", 500, 0.05);
+        m2.addLoan("Emily Hee", 25, 0.15);
+        m2.setAsPaid("Alice Betsy");
+        m2.listLoans();
+        System.out.println("profit from m2: " + m2.calculateProfitEarned());
     }
 }
